70ff3cf3b71ed95b717b80bc14c4b1ea6398b7d0
VNovik <novikgoit@gmail.com>
2016-05-12 15:54:49 +0300

    Erase comments

--- src/test/java/TnkLogin.java
+++ src/test/java/TnkLogin.java
@@ -5,12 +5,8 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class TnkLogin {
 
-    //public static void main(String[] args) {
-
-        @Test
+            @Test
         public void userCanLoginByUsername(){
             open("http://tnk-trans.com.ua");
         }
     }
-
-//}
