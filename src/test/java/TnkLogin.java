1decba66b0fcef90c0674c92830d3b2aebd139e8
Новик Виталий Ю <VYNovik@ua.tnk>
2016-05-12 15:48:20 +0300

    Initial commit

---
+++ src/test/java/TnkLogin.java
@@ -0,0 +1,16 @@
+
+import org.junit.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class TnkLogin {
+
+    //public static void main(String[] args) {
+
+        @Test
+        public void userCanLoginByUsername(){
+            open("http://tnk-trans.com.ua");
+        }
+    }
+
+//}
